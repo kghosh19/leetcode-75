0a39f903eb2a5d59c743dcbffcabad2c5694e599
kghosh19 <kathamrita19@gmail.com>
2026-06-20 09:15:48 +0530

    foundation

---
+++ src/main/java/com/leetcode/Solution.java
@@ -0,0 +1,7 @@
+package com.leetcode;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("LeetCode 75 Solutions");
+    }
+}
---
+++ src/test/java/com/leetcode/SolutionTest.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest {
+    @Test
+    public void testSolution() {
+        assertNotNull(new Solution());
+    }
+}
