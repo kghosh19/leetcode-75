5c2833e31fa79ba6b4f7a4aeacbbdb54031fee6c
kghosh19 <kathamrita19@gmail.com>
2026-06-24 10:08:19 +0530

    hash set problems

---
+++ src/main/java/com/leetcode/hashmaporset/DifferenceOfTwoArrays.java
@@ -0,0 +1,29 @@
+package com.leetcode.hashmaporset;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DifferenceOfTwoArrays {
+
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> s1 = Arrays.stream(nums1)
+                .boxed()
+                .collect(Collectors.toSet());
+        Set<Integer> s2 = Arrays.stream(nums2)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        List<Integer> diff1 = s1.stream()
+                .filter(n -> !s2.contains(n))
+                .toList();
+
+        List<Integer> diff2 = s1.stream()
+                .filter(n -> !s1.contains(n))
+                .toList();
+
+        return List.of(diff1, diff2);
+    }
+}
